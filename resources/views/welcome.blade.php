12c6569752a1418360646047be610704992cb698
michaelravedoni <michael@ravedoni.com>
2025-07-05 21:42:50 +0200

    up: welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,31 +20,31 @@
     @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/css/filament.css'])
 </head>
 
-<body class="flex min-h-screen items-center justify-center bg-stone-100 font-sans text-stone-800 antialiased dark:bg-[#161615] dark:text-[#EDEDEC]">
+<body class="bg-stone-100 dark:bg-[#0a0a0a] text-stone-500 flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
 
-    <div class="mx-auto max-w-4xl rounded-lg bg-white px-6 py-12 text-center shadow-lg">
+    <main class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
+    <div class="max-w-xl text-sm leading-sm flex-1 p-6 pb-12 lg:p-20 bg-white dark:bg-[#161615] dark:text-[#EDEDEC] shadow-lg rounded-es-lg rounded-ee-lg lg:rounded-ss-lg lg:rounded-ee-none">
         <div class="mb-8">
-            <img class="mx-auto h-20 w-20"
+            <img class="h-20 w-20"
                 src="/apple-touch-icon.png"
                 alt="Logo">
         </div>
-        <h1 class="mb-4 text-4xl font-extrabold text-stone-900">
-            Monitor
-        </h1>
-        <p class="mb-8 text-lg text-stone-600">
+        <h1 class="mb-1 text-xl font-medium text-stone-900">Monitor</h1>
+        <p class="mb-8">
             Application de monitoring pour athlète et entraîneurs.
         </p>
 
-        <div class="space-x-4">
-            <a class="inline-flex items-center rounded-md border border-stone-300 bg-white px-6 py-3 text-base font-medium text-stone-700 transition duration-150 ease-in-out hover:bg-stone-50 focus:outline-none focus:ring-2 focus:ring-stone-500 focus:ring-offset-2" href="#">
+        <div class="space-x-4 mx-auto">
+            <a class="text-center transition duration-150 ease-in-out hover:text-stone-900" href="#">
                 Admin
             </a>
         </div>
 
-        <div class="mt-12 text-sm text-stone-500">
+        <div class="mt-12 text-xs">
             <p>Made with ♥️ in Switzerland by <a class="underline" href="https://michaelravedoni.ch">Michael Ravedoni</a></p>
         </div>
     </div>
+</main>
 
 </body>
 
