aae557c7883d9500a3c1bbc2189c93fd39f1e1c7
Michael Ravedoni <30499597+michaelravedoni@users.noreply.github.com>
2025-11-12 19:44:31 +0100

    up: SendTrainerFeedbackCreated

--- app/Notifications/SendTrainerFeedbackCreated.php
+++ app/Notifications/SendTrainerFeedbackCreated.php
@@ -2,29 +2,29 @@
 
 namespace App\Notifications;
 
-use App\Models\Athlete;
 use App\Models\Feedback;
+use App\Models\Trainer;
 use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Notification;
 use NotificationChannels\WebPush\WebPushChannel;
 use NotificationChannels\Telegram\TelegramChannel;
 use NotificationChannels\Telegram\TelegramMessage;
 
-class SendAthleteFeedbackCreated extends Notification
+class SendTrainerFeedbackCreated extends Notification
 {
     use Queueable;
 
     protected $feedback;
 
-    protected $athlete;
+    protected $trainer;
 
     /**
      * Create a new notification instance.
      */
-    public function __construct(Feedback $feedback, Athlete $athlete)
+    public function __construct(Feedback $feedback, Trainer $trainer)
     {
         $this->feedback = $feedback;
-        $this->athlete = $athlete;
+        $this->trainer = $trainer;
     }
 
     /**
@@ -61,9 +61,9 @@ class SendAthleteFeedbackCreated extends Notification
     public function toTelegram(object $notifiable)
     {
         $message = TelegramMessage::create()
-            ->content('*'.$this->athlete?->name.'* a ajouté un feedback _'.$this->feedback?->type?->getLabel().'_ pour le '.$this->feedback?->date?->locale('fr_CH')->isoFormat('ll')." :\n".$this->feedback->content);
+            ->content('*'.$this->trainer?->name.'* a ajouté un feedback _'.$this->feedback?->type?->getLabel().'_ pour le '.$this->feedback?->date?->locale('fr_CH')->isoFormat('ll')." :\n".$this->feedback?->content);
 
-        $message->button('Voir', route('trainers.feedbacks.form', ['hash' => $notifiable->hash, 'd' => $this->feedback?->date->format('Y-m-d'), 'athlete_id' => $this->feedback?->athlete_id]));
+        $message->button('Voir', route('athletes.journal', ['hash' => $notifiable->hash]));
 
         return $message;
     }
--- app/Observers/FeedbackObserver.php
+++ app/Observers/FeedbackObserver.php
@@ -5,6 +5,7 @@ namespace App\Observers;
 use App\Models\Feedback;
 use Illuminate\Support\Facades\Notification;
 use App\Notifications\SendAthleteFeedbackCreated;
+use App\Notifications\SendTrainerFeedbackCreated;
 
 class FeedbackObserver
 {
@@ -14,12 +15,20 @@ class FeedbackObserver
     public function created(Feedback $feedback): void
     {
         $byAthlete = $feedback->author_type == 'athlete';
+        $byTrainer = $feedback->author_type == 'trainer';
 
         if ($byAthlete) {
             $athlete = $feedback->athlete;
             $trainers = $athlete->trainers;
             Notification::send($trainers, new SendAthleteFeedbackCreated($feedback, $athlete));
         }
+
+        if ($byTrainer) {
+            $athlete = $feedback->athlete;
+            $trainer = $feedback->trainer;
+            $athlete->notify(new SendTrainerFeedbackCreated($feedback, $trainer));
+        }
+
     }
 
     /**
